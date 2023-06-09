3f924b61bd515664c689ad7cd2c06e1ecd67aa03
simonbiennier <simon@biennier.fr>
2023-06-09 23:46:44 +0200

    ing server

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import curdir, sep
 
 hostName = "192.168.86.42"
 serverPort = 80
@@ -6,11 +7,29 @@ serverPort = 80
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        with open("./wwwroot/index.html", "rb") as f:
-            self.wfile.write(f.read())
+        if self.path == "/":
+            self.path = "/index.html"
+        try:
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype = "text/html"
+                sendReply = True
+            if self.path.endswith(".png"):
+                mimetype = "image/png"
+                sendReply = True
+            if self.path.endswith(".ico"):
+                mimetype = "image/webp"
+                sendReply = True
+
+            if sendReply:
+                with open(f"{curdir}{sep}{self.path}", "rb") as f:
+                    self.send_response(200)
+                    self.send_header("Content-type", mimetype)
+                    self.end_headers()
+                    self.wfile.write(f.read())
+            return
+        except IOError:
+            self.send_error(404, f"File Not Found: {self.path}")
 
 
 if __name__ == "__main__":
